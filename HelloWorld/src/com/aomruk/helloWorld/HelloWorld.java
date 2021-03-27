d46cbdb6bc2879a71e27985be43d7e41540f58ff
Tuengdin <78942120+Tuengdin@users.noreply.github.com>
2021-03-27 17:05:43 +0700

    First Commit

---
+++ HelloWorld/src/com/aomruk/helloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.aomruk.helloWorld;
+
+public class HelloWorld {
+	public static void main(String [] args) {
+		System.out.print("Hello World");
+	}
+
+}
---
+++ HelloWorld/src/com/aomruk/helloWorld/Lab1.java
@@ -0,0 +1,25 @@
+package com.aomruk.helloWorld;
+/*
+ * Java Lab 1
+ * 1. Comment in single line and Multiple line
+ * This comment are in Multiple Line   
+ */
+public class Lab1 {
+	
+// 2. Declaration int, boolean String, double and char
+	public static void main(String[] args) {
+		int intLab = 10;
+		boolean booleanLab = true;
+		String StringLab = "Hello Lab1";
+		double doubleLab = 0.5;
+		char charLab = 'A';
+		
+// 3. Print variable from 2. by "This is a String" + variable 
+		System.out.println("This is a int: " + intLab);
+		System.out.println("This is a boolean: " + booleanLab);
+		System.out.println("This is a String: " + StringLab);
+		System.out.println("This is a double: " + doubleLab);
+		System.out.println("This is a char: " + charLab);
+	}
+
+}
